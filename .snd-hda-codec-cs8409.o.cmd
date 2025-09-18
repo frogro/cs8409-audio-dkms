62a55ce1c11220f34e2be02df2964e0f46fa3ec1
frogro <frankiin77@hotmail.com>
2025-09-18 16:32:41 +0200

    Delete .snd-hda-codec-cs8409.o.cmd

--- .snd-hda-codec-cs8409.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/frank/Dokumente/jioiij/mic/snd-hda-codec-cs8409.o := ld -m elf_x86_64 -z noexecstack --no-warn-rwx-segments   -r -o /home/frank/Dokumente/jioiij/mic/snd-hda-codec-cs8409.o @/home/frank/Dokumente/jioiij/mic/snd-hda-codec-cs8409.mod 
